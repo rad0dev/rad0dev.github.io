ce025b061b31f037b2fa553b3b445320cd4c79cd
radoslawolejniczak <r.olejniczak@asper.net.pl>
2020-03-16 17:49:06 +0100

    webpack config fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'production',
@@ -9,8 +9,43 @@ module.exports = {
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html'
-    })
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/repositories/index.html',
+      filename: "repositories/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/contact/index.html',
+      filename: "contact/index.html"
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/skills/index.html',
+      filename: "skills/index.html"
+    }),
+    new CopyPlugin([
+      {
+        from: path.resolve(__dirname, 'src/img'),
+        to: path.resolve(__dirname, 'dist/img')
+      }
+    ]),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            },
+          },
+          'sass-loader'
+        ]
+      },
+    ],
+  },
   optimization: {
     runtimeChunk: "single",
     splitChunks: {
